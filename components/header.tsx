b30df3ce79b1dba389c98117536e7ff10d0b2188
zoosos <wbin90620@gmail.com>
2025-02-02 13:49:44 +0800

    changed header logo size

--- components/header.tsx
+++ components/header.tsx
@@ -9,7 +9,7 @@ export const Header: React.FC = async () => {
     <header className="fixed w-full p-2 flex justify-between items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent">
       <div>
         <a href="/">
-          <IconLogo className={cn('w-5 h-5')} />
+          <IconLogo className={cn('w-20 h-20')} />
           <span className="sr-only">Hanzo Chat</span>
         </a>
       </div>
